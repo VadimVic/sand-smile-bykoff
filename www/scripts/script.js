97d0dc201f8318732065286e50fa0429e344706d
Vadim Bykov <nordvadim@bk.ru>
2021-06-25 13:58:19 +0300

    худо-бедно вставил слайдер

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,9 @@
-console.log('script work!');
+  $(document).ready(function(){
+    $('.js-click-slider').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 1
+    });
+  });
+
+
