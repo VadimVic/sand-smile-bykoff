e4e0ebab1e53af783bc5c201fd682929b1d48ef2
Vadim Bykov <nordvadim@bk.ru>
2021-06-28 13:55:11 +0300

    застилизовал checkbox и products

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,26 +1,27 @@
   $(document).ready(function(){
     $('.js-slider').slick({
       dots: true,
+      autoplay: true,
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1
     });
 
-  // // // Делаем карусель в секции photos
+  // // // // Делаем карусель в секции photos
   // $('.js-click-slider').each(function (index, sliderWrap) {
   //   let slider = $(sliderWrap).find('.js-slider');
   //   let btnPrev = $(sliderWrap).find('.js-btn-prev');
   //   let btnNext = $(sliderWrap).find('.js-btn-next');
 
-  // //   $(slider).slick({
-  // //     autoplay: true,
-  // //     dots: true,
-  // //     prevArrow: btnPrev,
-  // //     nextArrow: btnNext,
-  // //   });
+  //   $(slider).slick({
+  //     autoplay: true,
+  //     dots: true,
+  //     prevArrow: btnPrev,
+  //     nextArrow: btnNext,
+  //   });
 
-  // //   // console.log(index);
-  // //   // console.log(sliderWrap);
+  // // //   // console.log(index);
+  // // //   // console.log(sliderWrap);
   // });
 
   });
