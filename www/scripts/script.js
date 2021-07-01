fb31c8f3a67251e38c580e68ed4a2141135fd936
Vadim Bykov <nordvadim@bk.ru>
2021-07-01 20:26:35 +0300

    последняя версия TEAM без ссылок на фото

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,28 +1,27 @@
   $(document).ready(function(){
-    $('.js-slider').slick({
-      dots: true,
+    // $('.js-slider').slick({
+    //   dots: true,
+    //   autoplay: true,
+    //   infinite: true,
+    //   slidesToShow: 1,
+    //   slidesToScroll: 1
+    // });
+
+  // // // // Делаем карусель в секции photos
+  $('.js-click-slider').each(function (index, sliderWrap) {
+    let slider = $(sliderWrap).find('.js-slider');
+    let btnPrev = $(sliderWrap).find('.js-btn-prev');
+    let btnNext = $(sliderWrap).find('.js-btn-next');
+
+    $(slider).slick({
       autoplay: true,
-      infinite: true,
+      dots: true,
+      prevArrow: btnPrev,
+      nextArrow: btnNext,
       slidesToShow: 1,
       slidesToScroll: 1
     });
-
-  // // // // Делаем карусель в секции photos
-  // $('.js-click-slider').each(function (index, sliderWrap) {
-  //   let slider = $(sliderWrap).find('.js-slider');
-  //   let btnPrev = $(sliderWrap).find('.js-btn-prev');
-  //   let btnNext = $(sliderWrap).find('.js-btn-next');
-
-  //   $(slider).slick({
-  //     autoplay: true,
-  //     dots: true,
-  //     prevArrow: btnPrev,
-  //     nextArrow: btnNext,
-  //   });
-
-  // // //   // console.log(index);
-  // // //   // console.log(sliderWrap);
-  // });
+  });
 
   $('.burger').on('click', function () {
     $('.main-nav').slideToggle();
@@ -30,14 +29,25 @@
 
 
 
-  $('.team_items a p').on('click', function (e) {
+  $('.team_items a').on('click', function (e) {
     e.preventDefault();
 
-    let index = $(this).index('.team_items a p');
+    let index = $(this).index('.team_items a');
     console.log(index);
 
-    $('.team_items a p').removeClass('active');
+    $('.team_items a').removeClass('active');
     $(this).addClass('active');
+
+
+    $('.team_items p').on('click', function (e) {
+      e.preventDefault();
+
+    let index = $(this).index('.team_items p');
+    console.log(index);
+
+    $('.team_items p').removeClass('active');
+    $(this).addClass('active');
+    });
 
     $('.team-person').removeClass('team-person-visible');
     $('.team-person').eq(index).addClass('team-person-visible');
